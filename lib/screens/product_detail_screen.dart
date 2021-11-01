b419776425f27fe31b2061f1351c56c4c22402d7
Santiago Bonessi <santiagobonessi@hotmail.com>
2021-11-01 21:02:21 +1000

    Add custom scroll view in product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -13,37 +13,46 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments as String;
     final product = Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(product.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Hero(
+                tag: product.id,
                 child: Image.network(
                   product.imageUrl,
                   fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 10),
-              Text(
-                '\$${product.price}',
-                style: TextStyle(color: Colors.grey, fontSize: 20),
-              ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: double.infinity,
-                child: Text(
-                  '${product.description}',
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                ),
-              ),
-            ],
+            ),
           ),
-        ));
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10),
+                Text(
+                  '\$${product.price}',
+                  style: TextStyle(color: Colors.grey, fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  child: Text(
+                    '${product.description}',
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
